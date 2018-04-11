3227dfd81e8572d887a1a5d356b252f0fd796f1e
EM\APatil <ashish.patil@elliemae.com>
2018-04-10 17:28:08 -0700

    Enhanced logging

--- redis/redis-base.js
+++ redis/redis-base.js
@@ -4,12 +4,10 @@ const bluebird = require('bluebird');
 bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
 
-/*
 const redisConnectionOptions = {
     url: 'redis://cache-apigateway-dev.in4bqk.ng.0001.usw2.cache.amazonaws.com:6379'
 };
-*/
 
-const client = redis.createClient();
+const client = redis.createClient(redisConnectionOptions);
 
 module.exports = {redisClient: client};
--- redis/redis-list.js
+++ redis/redis-list.js
@@ -1,16 +1,17 @@
 const client = require('./redis-base').redisClient;
 
 // const key = 'limit.concurrency.3010000024';
-const key = 'quota.daily.3010000024';
+// const key = 'quota.daily.3010000024';
 
-client.llenAsync(key).then((count) => {
-    console.log(`count before deletion: ${count}`);
-    return client.delAsync(key);
-}).then((removedCount) => {
-    console.log(`removedCount : ${removedCount}`);
-    return client.existsAsync(key);
-}).then((exists) => {
-    console.log(`exists: ${exists}`);
+//For BE11205048, BE11205049
+// const key = 'quota.daily.3011205048';
+
+//For BE11205050
+const key = 'quota.daily.3011204756';
+
+
+client.existsAsync(key).then((exists) => {
+    console.log(`key ${key} exists: ${exists}`);
     return client.pttlAsync(key)
 }).then((ttl) => {
     console.log(`time to live ${ttl} milliseconds`);
@@ -19,11 +20,17 @@ client.llenAsync(key).then((count) => {
     console.log(`expiryTime : ${expiryTime}`);
     let date = new Date();
     let time = date.setTime(expiryTime);
-    console.log(`expiry in UTC : ${date.toUTCString()}`)
+    console.log(`expiry in UTC : ${date.toUTCString()}`);
+    return client.llenAsync(key);
+}).then((count) => {
+    console.log(`count before deletion: ${count}`);
+    return client.delAsync(key);
+}).then((removedCount) => {
+    console.log(`removedCount : ${removedCount}`);
 });
 
 /*client.existsAsync(key).then((exists) => {
-    console.log(`exists: ${exists}`);
+    console.log(`key ${key} exists: ${exists}`);
     return client.pttlAsync(key)
 }).then((ttl) => {
     console.log(`time to live ${ttl} milliseconds`);
