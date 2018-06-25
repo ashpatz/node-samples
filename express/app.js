6634b0e8710ddc1b5b412059a5fa32d70ccafe5b
ashpatz <007.ashish@gmail.com>
2018-06-25 12:10:43 -0700

    Adding a gzip endpoint

--- express/app.js
+++ express/app.js
@@ -1,14 +1,18 @@
 'use strict';
 
 const express = require('express');
+const compression = require('compression');
 const auditServiceClient = require('./service/audit-service-client');
+const fileService = require('./service/file-service');
 const consoleLog = require('../logger/logger').consoleLog;
 
-const PORT = 3000;
+const PORT = 3001;
 
 const app = express();
+app.use(compression());
 app.post('/log', auditServiceClient.pushAndFetch);
 app.post('/es', auditServiceClient.createInES);
+app.get('/file', fileService.readFile);
 
 app.listen(PORT, () => {
     consoleLog(`App now listening to requests on port ${PORT}`);
