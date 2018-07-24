148e88c5873f26e0b2f7c62b10d656f67ff872ca
ashpatz <007.ashish@gmail.com>
2018-07-23 17:39:48 -0700

    Setting cookie via response

--- express/service/file-service.js
+++ express/service/file-service.js
@@ -4,9 +4,20 @@ const hugeJson = require('./huge_file.json');
 
 module.exports.readFile = (request, response) => {
 
+    console.log(request.get('Cookie'));
+
+    const cookieOptions = {
+        domain: '.example.com',
+        path: '/admin',
+        secure: true,
+        expires: new Date(Date.now() + 900000),
+        httpOnly: true
+    };
+
     response
         .status(200)
         .set('content-type', 'application/json')
+        .cookie('AWSALB', 'sJ3zBtnffoC/HOIm1TfAhVYFDDBiPprvb/BrRtv1l/j2LOZTRXREnLKOumKKZMrH1PPlsNIxlxNinfr3TNQR1k+ncW0DxeGum+2XLuYtHCcQdFsVwYuYfvyP9zJR', cookieOptions)
         .send(hugeJson);
 
 };
